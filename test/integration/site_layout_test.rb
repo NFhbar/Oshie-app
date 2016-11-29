de7114b9fe2a89e739b667da22b0b348d9688801
NFhbar <frega.nicolas@gmail.com>
2016-11-29 10:44:09 -0800

    small testing

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,6 +8,25 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", policy_privacy_path
     assert_select "a[href=?]", policy_privacy_path(:anchor => "cookies")
 
+    get policy_promise_path
+    assert_template 'policy/promise'
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", policy_privacy_path
+    assert_select "a[href=?]", policy_aup_path
+
+    get policy_privacy_path
+    assert_template 'policy/privacy'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", policy_promise_path
+    assert_select "a[href=?]", policy_aup_path
+
+    get policy_aup_path
+    assert_template 'policy/acceptable'
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", policy_promise_path
+    assert_select "a[href=?]", policy_privacy_path
+
+
   end
 
 end
